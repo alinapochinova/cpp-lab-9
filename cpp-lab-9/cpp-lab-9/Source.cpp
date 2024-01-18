802d781f8274b0bed088cf94c3a8eee1e59b94e8
alinapochinova <alinapochinova@mail.ru>
2024-01-18 22:00:50 +0300

    Create Source.cpp

---
+++ cpp-lab-9/cpp-lab-9/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+template <typename T, typename F>
+int countElements(T* arr, int size, F condition) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (condition(arr[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    // Примеры использования функции
+
+    // Массив целых чисел
+    int intArr[] = { 1, 2, 3, 4, 5 };
+    int intCount = countElements(intArr, 5, [](int num) { return num > 2; });
+    std::cout << "Number of elements greater than 2: " << intCount << std::endl;
+
+    // Массив чисел с плавающей запятой
+    double doubleArr[] = { 1.5, 2.7, 3.2, 4.1, 5.9 };
+    int doubleCount = countElements(doubleArr, 5, [](double num) { return num < 3.0; });
+    std::cout << "Number of elements less than 3.0: " << doubleCount << std::endl;
+    
+    // Массив символов
+    char charArr[] = { 'a', 'b', 'c', 'd', 'e' };
+    int charCount = countElements(charArr, 5, [](char ch) { return ch != 'a'; });
+    std::cout << "Number of elements not equal to 'a': " << charCount << std::endl;
+
+    return 0;
+}
